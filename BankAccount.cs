51eb178b8a5a4dee286236cfa721629c56dcb7dc
jeeban <jeebanjyoti193@gmail.com>
2023-07-26 16:12:20 +0530

    first commit

---
+++ BankAccount.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Day_13_Assignment_16
+{
+    public class BankAccount
+    {
+        private static int lastAccountNumber = 0;
+
+        public string AccountNumber { get; }
+        public string AccountHolderName { get; set; }
+
+        private double balance;
+        public double Balance
+        {
+            get { return balance; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Balance cannot be negative.");
+                }
+                balance = value;
+            }
+        }
+
+        public BankAccount(string accountHolderName)
+        {
+            AccountNumber = GenerateAccountNumber();
+            AccountHolderName = accountHolderName;
+            Balance = 0;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Deposit amount must be greater than zero.");
+            }
+
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdrawal amount must be greater than zero.");
+            }
+
+            if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient balance.");
+            }
+
+            Balance -= amount;
+        }
+
+        public void PrintAccountDetails()
+        {
+            Console.WriteLine($"Account Number: {AccountNumber}");
+            Console.WriteLine($"Account Holder Name: {AccountHolderName}");
+            Console.WriteLine($"Balance: {Balance}");
+            Console.WriteLine();
+        }
+
+        private string GenerateAccountNumber()
+        {
+            lastAccountNumber++;
+            return $"ACCT-{lastAccountNumber}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Day_13_Assignment_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the account holder's name: ");
+            string accountHolderName = Console.ReadLine();
+
+            BankAccount account = new BankAccount(accountHolderName);
+
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("Bank Account ");
+                Console.WriteLine($"\nChoose your desired option {account.AccountHolderName}:");
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter your choice: ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Write("Enter the amount to deposit: ");
+                            if (double.TryParse(Console.ReadLine(), out double depositAmount))
+                            {
+                                try
+                                {
+                                    account.Deposit(depositAmount);
+                                    Console.WriteLine($"Deposited {depositAmount} successfully.");
+                                    account.PrintAccountDetails();
+                                }
+                                catch (ArgumentException ex)
+                                {
+                                    Console.WriteLine("Error: " + ex.Message);
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid input for deposit amount.");
+                            }
+                            break;
+
+                        case 2:
+                            Console.Write("Enter the amount to withdraw: ");
+                            if (double.TryParse(Console.ReadLine(), out double withdrawAmount))
+                            {
+                                try
+                                {
+                                    account.Withdraw(withdrawAmount);
+                                    Console.WriteLine($"Withdrawn {withdrawAmount} successfully.");
+                                    account.PrintAccountDetails();
+                                }
+                                catch (ArgumentException ex)
+                                {
+                                    Console.WriteLine("Error: " + ex.Message);
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid input for withdrawal amount.");
+                            }
+                            break;
+
+                        case 3:
+                            exit = true;
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid choice. Please choose a valid option.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid choice.");
+                }
+            }
+
+            Console.WriteLine("\nFinal Account Details:");
+            account.PrintAccountDetails();
+        }
+    }
+}
